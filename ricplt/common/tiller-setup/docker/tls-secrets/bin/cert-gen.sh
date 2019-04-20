fcaaea8918aa9ed699a63b7ef36cc0841b83e9e8
dave kormann <dk3239@att.com>
2019-04-20 16:01:59 -0400

    WIP: IMPLEMENTS: RICPLT-1144: Support multiple Tillers
    This change adds scripts and configurations to support multiple,
    limited-capability and TLS-enabled helm tiller instances in the RIC.
    Specifically, it's envisioned that a RIC will have (at least) two
    running tiller pods: one for xapp deployment and one for ric platform
    component deployment; these will be independent of any systemwide
    tiller deployment, and will be limited to acting within their own
    namespaces.
    
    Change-Id: I39341a6191c688d1960b5bfbe47eb99ca95fa1d6
    signed-off-by: dave kormann <dk3239@att.com>

---
+++ ricplt/common/tiller-setup/docker/tls-secrets/bin/cert-gen.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+dnBase=${CERT_DN:-'/C=US/O=O-RAN Alliance/OU=O-RAN Software Community'}
+keyBits=${KEY_BITS:-4096}
+
+CAHome=${CA_DIR:-'/pki'}
+CADays=${CA_CERT_EXPIRY:-9125}
+CAKey=${CAHome}/${CA_KEY_NAME:-ca.key.pem}
+CACert=${CAHome}/${CA_CERT_NAME:-ca.cert.pem}
+#
+CertHome=${CERT_DIR:-$CAHome}
+#
+TillerDays=${TILLER_CERT_EXPIRY:-3650}
+TillerKey=${CertHome}/${TILLER_KEY_NAME:-tiller.key.pem}
+TillerCert=${CertHome}/${TILLER_CERT_NAME:-tiller.cert.pem}
+TillerCN=${TILLER_CN:-tiller}
+#
+HelmDays=${HELM_CERT_EXPIRY:-3650}
+HelmKey=${CertHome}/${HELM_KEY_NAME:-helm.key.pem}
+HelmCert=${CertHome}/${HELM_CERT_NAME:-helm.cert.pem}
+HelmCN=${HELM_CN:-helm}
+
+# 1. CA
+if [ ! -d ${CAHome} ]; then
+  mkdir -p ${CAHome}
+fi
+if [ ! -f ${CAKey} ]; then
+  openssl genrsa -out ${CAKey} ${keyBits}
+fi
+if [ ! -f ${CACert} ]; then
+ openssl req -new -x509 -extensions v3_ca -sha256 -days ${CADays} \
+  -key ${CAKey} \
+  -out ${CACert} \
+  -subj "${dnBase}" 
+fi
+
+# 2. tiller server cert
+if [ ! -f ${TillerKey} ]; then
+ openssl genrsa -out ${TillerKey} ${keyBits}
+fi
+if [ ! -f ${TillerCert} ]; then
+ CSR=`mktemp`
+ openssl req -new -sha256 \
+  -key ${TillerKey} \
+  -out ${CSR} \
+  -subj "${dnBase}/CN=${TillerCN}"
+ openssl x509 -req -CAcreateserial -days ${TillerDays} \
+  -CA ${CACert} \
+  -CAkey ${CAKey} \
+  -in ${CSR} \
+  -out ${TillerCert}
+fi
+
+# 3. helm client cert
+if [ ! -f ${HelmKey} ]; then
+ openssl genrsa -out ${HelmKey} ${keyBits}
+fi
+if [ ! -f ${HelmCert} ]; then
+ CSR=`mktemp`
+ openssl req -new -sha256 \
+  -key ${HelmKey} \
+  -out ${CSR} \
+  -subj "${dnBase}/CN=${HelmCN}"
+ openssl x509 -req -CAcreateserial -days ${HelmDays} \
+  -CA ${CACert} \
+  -CAkey ${CAKey} \
+  -in ${CSR} \
+  -out ${HelmCert}
+fi

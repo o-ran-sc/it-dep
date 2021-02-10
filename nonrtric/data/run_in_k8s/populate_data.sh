9e966f65fdcd0349b25a8a472b4553d31aa6d834
Zhe Huang <zhehuang@research.att.com>
2021-02-10 13:04:52 +0000

    Revert "Merge branch 'master' into cherry"
    This reverts commit de7eb1f8a01edd37e85bed1edea5777c672d49c7.
    
    Reason for revert: The new nonrtric helm charts are intended for the D release.
    
    Change-Id: I69fec06935f57499e174fb6fd321343988404de7
    Signed-off-by: Zhe Huang <zhehuang@research.att.com>

--- nonrtric/data/run_in_k8s/populate_data.sh
+++ nonrtric/data/run_in_k8s/populate_data.sh
@@ -23,8 +23,6 @@
 # one service in policy agent
 # five policies in a1-sim-OSC_0
 # one policy in a1-sim-STD_0
-# one policy type in a1-sim-STD2_0
-# one policy in a1-sim-STD2_0
 
 policy_agent_host="policymanagementservice"
 policy_agent_port="9080"
@@ -36,10 +34,6 @@ a1_sim_std_0_host="a1-sim-std-0.a1-sim"
 a1_sim_std_0_port="8085"
 a1_sim_std_1_host="a1-sim-std-1.a1-sim"
 a1_sim_std_1_port="8085"
-a1_sim_std2_0_host="a1-sim-std2-0.a1-sim"
-a1_sim_std2_0_port="8085"
-a1_sim_std2_1_host="a1-sim-std2-1.a1-sim"
-a1_sim_std2_1_port="8085"
 httpx=http
 
 echo "policy agent status:"
@@ -62,14 +56,6 @@ echo "ric4 version:"
 curl -skw " %{http_code}" $httpx://$a1_sim_std_1_host:$a1_sim_std_1_port/counter/interface
 echo -e "\n"
 
-echo "ric5 version:"
-curl -skw " %{http_code}" $httpx://$a1_sim_std2_0_host:$a1_sim_std_0_port/counter/interface
-echo -e "\n"
-
-echo "ric6 version:"
-curl -skw " %{http_code}" $httpx://$a1_sim_std2_1_host:$a1_sim_std_1_port/counter/interface
-echo -e "\n"
-
 echo "create policy type 1 to ric1:"
 curl -X PUT -skw " %{http_code}" $httpx://$a1_sim_osc_0_host:$a1_sim_osc_0_port/policytype?id=1 -H Content-Type:application/json --data-binary @testdata/OSC/policy_type.json
 echo -e "\n"
@@ -78,20 +64,12 @@ echo "create policy type 1 to ric2:"
 curl -X PUT -skw " %{http_code}" $httpx://$a1_sim_osc_1_host:$a1_sim_osc_1_port/policytype?id=1 -H Content-Type:application/json --data-binary @testdata/OSC/policy_type.json
 echo -e "\n"
 
-echo "create policy type 2 to STD2 ric5:"
-curl -X PUT -skw " %{http_code}" $httpx://$a1_sim_std2_0_host:$a1_sim_std2_0_port/policytype?id=2 -H Content-Type:application/json --data-binary @testdata/STD/policy_type.json
-echo -e "\n"
-
-echo "create policy type 2 to STD2 ric6:"
-curl -X PUT -skw " %{http_code}" $httpx://$a1_sim_std2_1_host:$a1_sim_std2_1_port/policytype?id=2 -H Content-Type:application/json --data-binary @testdata/STD/policy_type.json
-echo -e "\n"
-
 for i in {1..12}; do
 	echo "policy types from policy agent:"
     curlString="curl -skw %{http_code} $httpx://$policy_agent_host:$policy_agent_port/policy_types"
     res=$($curlString)
     echo "$res"
-    expect="[\"\",\"1\",\"2\"]200"
+    expect="[\"\",\"1\"]200"
     if [ "$res" == "$expect" ]; then
         echo -e "\n"
         break;
@@ -122,10 +100,6 @@ echo -e "\n"
 
 echo "create policy 2004 to ric1 with type1 and service1 via policy agent:"
 curl -k -X PUT -sw " %{http_code}" -H accept:application/json -H Content-Type:application/json "$httpx://$policy_agent_host:$policy_agent_port/policy?id=2004&ric=ric1&service=service1&type=1" --data '{"scope": {"ueId": "ue3100","qosId": "qos3100"},"qosObjective": {"priorityLevel": 3104}}'
-echo -e "\n"
-
-echo "create policy 2200 to ric5 with type1 and service1 via policy agent:"
-curl -k -X PUT -sw " %{http_code}" -H accept:application/json -H Content-Type:application/json "$httpx://$policy_agent_host:$policy_agent_port/v2/policies" --data-binary @testdata/STD/policy.json
 echo -e "\n"
 
 echo "policy numbers from ric1:"

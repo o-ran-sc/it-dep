a2c2df5014145ee3126146987d9039e397b12407
sebdet <sebastien.determe@intl.att.com>
2021-10-15 13:47:31 +0200

    First draft of the SMO package based on the ONAP charts
    2 git submodules pointing to Onap oom & MulticloudK8S
    + scripts to setup K8s node/Build charts/Installation
    + Helm override for helm config used during the installation
    + Oran charts reworked so that they can be built like in ONAP OOM
    + For the verify Job: remove smo-install from dockerfile (another verify job will be added)
    
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    Signed-off-by: xuegao <xue.gao@intl.att.com>
    Issue-ID: NONRTRIC-609
    Change-Id: I93425022761bc4e7efce2bd374fb9ef101942fc0

---
+++ smo-install/scripts/layer-0/0-setup-microk8s.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+###
+# ============LICENSE_START=======================================================
+# ORAN SMO Package
+# ================================================================================
+# Copyright (C) 2021 AT&T Intellectual Property. All rights
+#                             reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END============================================
+# ===================================================================
+# 
+###
+
+## Microk8S part
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+snap remove microk8s
+snap remove kubectl
+snap install microk8s --classic --channel=1.22/stable
+sudo snap install kubectl --classic --channel=1.22/stable
+
+## Firewall
+ufw allow in on cni0 && sudo ufw allow out on cni0
+ufw default allow routed
+
+## Enable required features for K8S
+microk8s enable dns storage
+
+## Setup kubectl
+cd
+mkdir .kube
+cd .kube
+sudo microk8s.config > config
+chmod 700 config
+
+#Check the install
+echo "Checking Kubernetes ..."
+kubectl version

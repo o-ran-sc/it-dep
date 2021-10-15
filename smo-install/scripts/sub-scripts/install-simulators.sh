c5d4450f19194018b2b9b050e42c82b0f2a50f31
sebdet <sebastien.determe@intl.att.com>
2021-10-15 13:47:31 +0200

    First draft of the SMO package based on the ONAP charts
    2 git submodules pointing to Onap oom & MulticloudK8S
    + scripts to setup K8s node/Build charts/Installation
    + Helm override for helm config used during the installation
    + Oran charts reworked so that they can be built like in ONAP OOM
    + For the verify Job: remove smo-install from dockerfile (another verify job will be added)
    
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    Signed-off-by: xuegao <xue.gao@intl.att.com>
    Issue-ID: NONRTRIC-609
    Change-Id: I93425022761bc4e7efce2bd374fb9ef101942fc0

---
+++ smo-install/scripts/sub-scripts/install-simulators.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+###
+# ============LICENSE_START=======================================================
+# ORAN SMO Package
+# ================================================================================
+# Copyright (C) 2021 AT&T Intellectual Property. All rights
+#                             reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END============================================
+# ===================================================================
+# 
+###
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage:  install-simulators.sh <helm-override-file-path><host-ip>'
+    echo 'Example: install-simulators.sh ../Helm-override/simulators.yaml 192.168.1.46'
+    exit 1
+fi
+
+kubectl create namespace network
+echo '### Installing ORAN SIMULATORS part ###'
+helm install --debug oran-simulator local/ru-du-simulators --namespace network -f $1 --set ru-simulator.ntsimNg.sdnControllerIp=$2 --set ru-simulator.ntsimNg.vesEndpointIp=$2 --set du-simulator.ntsimNg.sdnControllerIp=$2 --set du-simulator.ntsimNg.vesEndpointIp=$2 --set topology-server.ntsimNg.sdnControllerIp=$2 --set topology-server.ntsimNg.vesEndpointIp=$2

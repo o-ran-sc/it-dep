f6ee773f0f2fb3c191593de505e62cb2229baa3d
DenisGNoonan <denis.noonan@est.tech>
2024-05-16 10:49:21 +0100

    NONRTRIC-980: Service Manager - SMO deployment
    Issue-ID: NONRTRIC-980
    Change-Id: I7be646a7deef5bafc20c6cb0a2951f82564320b4
    Signed-off-by: DenisGNoonan <denis.noonan@est.tech>

--- smo-install/scripts/sub-scripts/uninstall-nonrtric.sh
+++ smo-install/scripts/sub-scripts/uninstall-nonrtric.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 ###
-# ============LICENSE_START=======================================================
+# ============LICENSE_START========================================================
 # ORAN SMO Package
-# ================================================================================
+# =================================================================================
 # Copyright (C) 2021 AT&T Intellectual Property. All rights
 #                             reserved.
-# ================================================================================
+# Modification Copyright (C) 2024 OpenInfra Foundation Europe. All rights reserved.
+# =================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -18,10 +19,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ============LICENSE_END============================================
-# ===================================================================
-# 
+# ============LICENSE_END==========================================================
+# =================================================================================
+#
 ###
+
+if ! jq --version > /dev/null 2>&1 ; then
+    sudo apt-get update
+    sudo apt-get install -y jq
+fi
+
+INSTALL_KONG=$(helm get values oran-nonrtric -n nonrtric -o json | jq '.nonrtric.installKong')
+if [ $? -ne 0 ]; then
+    echo "Failed to parse helm release value installKong with jq."
+    exit 1
+fi
+
+if [ "$INSTALL_KONG" = true ];then
+    echo "Warning - deleting Kong routes and services for ServiceManager."
+    SERVICEMANAGER_POD=$(kubectl get pods -o custom-columns=NAME:.metadata.name -l app.kubernetes.io/name=servicemanager --no-headers -n nonrtric)
+    if [[ -n $SERVICEMANAGER_POD ]]; then
+        kubectl exec $SERVICEMANAGER_POD -n nonrtric -- ./kongclearup
+    else
+        echo "Error - Servicemanager pod not found, didn't delete Kong routes and services for ServiceManager."
+    fi
+fi
 
 kubectl delete namespace nonrtric
 kubectl delete pv nonrtric-pv2

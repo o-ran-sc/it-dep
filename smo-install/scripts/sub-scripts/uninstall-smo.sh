f014a217c4b34e30eab13fb404c44959c2be61ef
aravind.est <aravindhan.a@est.tech>
2024-06-21 15:37:32 +0100

    Add TEIV helm charts
    TEIV Helm charts added
    
    Issue-ID: SMO-154
    Change-Id: Id8fa68200993306f3ac69463a14ab0b1b4482c2b
    Signed-off-by: aravind.est <aravindhan.a@est.tech>

--- smo-install/scripts/sub-scripts/uninstall-smo.sh
+++ smo-install/scripts/sub-scripts/uninstall-smo.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
-###
 # ============LICENSE_START=======================================================
-# ORAN SMO Package
-# ================================================================================
-# Copyright (C) 2021 AT&T Intellectual Property. All rights
-#                             reserved.
+# Copyright (C) 2024 OpenInfra Foundation Europe. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,16 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============LICENSE_END============================================
-# ===================================================================
-# 
-###
+#
 
-SCRIPT=$(readlink -f "$0")
-SCRIPT_PATH=$(dirname "$SCRIPT")
-cd $SCRIPT_PATH
-
-sudo apt-get install make -y
-
-echo  '### Building ORAN part ###'
-(cd ../../oran_oom && make all)
-(cd ../../oran_oom/smo && make all)
+kubectl delete namespace smo
+kubectl get pv | grep Released | awk '$1 {print$1}' | while read vol; do kubectl delete pv/${vol}; done

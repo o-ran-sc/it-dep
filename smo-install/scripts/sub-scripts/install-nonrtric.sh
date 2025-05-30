d8d5422aa17b18acc46a1f84910d32d7534dbf2a
aravind.est <aravindhan.a@est.tech>
2025-05-30 18:16:25 +0100

    Fix minor onap installation issues
    ONAP release instllation helm snapshots updated to latest.
    
    Issue-ID: NONRTRIC-1020
    Change-Id: Id350949496ec3450eca88abfcb68c394c9ceac66
    Signed-off-by: aravind.est <aravindhan.a@est.tech>

--- smo-install/scripts/sub-scripts/install-nonrtric.sh
+++ smo-install/scripts/sub-scripts/install-nonrtric.sh
@@ -45,7 +45,7 @@ fi
 
 if [ "$MODE" == "dev" ]; then
     echo "Installing NONRTRIC in dev mode"
-    helm install --debug oran-nonrtric local/nonrtric --namespace nonrtric -f $OVERRIDEYAML --set nonrtric.persistence.mountPath="/dockerdata-nfs/deployment-$3" --set kong.kongpv.enable=$kongPvEnabled
+    helm install --debug oran-nonrtric local/nonrtric --namespace nonrtric -f $OVERRIDEYAML --set nonrtric.persistence.mountPath="/dockerdata-nfs/deployment-$3" --set kong.kongpv.enabled=$kongPvEnabled
 else
     echo "Installing NONRTRIC in release mode"
     # This following should be modified once the charts are uploaded and available in the nexus repository
@@ -53,7 +53,7 @@ else
         # helm repo add nonrtric https://nexus3.o-ran-sc.org/repository/smo-helm-snapshots/
         # helm repo update
         # helm install oran-nonrtric nonrtric/nonrtric --namespace nonrtric -f $OVERRIDEYAML --create-namespace
-    helm install oran-nonrtric local/nonrtric --namespace nonrtric -f $OVERRIDEYAML --set nonrtric.persistence.mountPath="/dockerdata-nfs/deployment-$3" --set kong.kongpv.enable=$kongPvEnabled
+    helm install oran-nonrtric local/nonrtric --namespace nonrtric -f $OVERRIDEYAML --set nonrtric.persistence.mountPath="/dockerdata-nfs/deployment-$3" --set kong.kongpv.enabled=$kongPvEnabled
 fi
 
 

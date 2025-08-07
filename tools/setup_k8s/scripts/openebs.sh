d8414c08540914180a8ca949a7e3f6b648698f1c
Bimo Fransiscus Asisi <d11002806@gapps.ntust.edu.tw>
2025-08-07 16:54:16 +0800

    add openebs as default local storageclass. Required by
    issue-id: INT-192
    Change-Id: Ie5cb8fd0a640905a1f0eac24e0138c3fcf46188b
    Signed-off-by: Bimo Fransiscus Asisi <d11002806@gapps.ntust.edu.tw>

---
+++ tools/setup_k8s/scripts/openebs.sh
@@ -0,0 +1,29 @@
+################################################################################
+#   Copyright (c) 2025 Broadband Multimedia Wireless Lab, NTUST                #                                #
+#                                                                              #
+#   Licensed under the Apache License, Version 2.0 (the "License");            #
+#   you may not use this file except in compliance with the License.           #
+#   You may obtain a copy of the License at                                    #
+#                                                                              #
+#       http://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                              #
+#   Unless required by applicable law or agreed to in writing, software        #
+#   distributed under the License is distributed on an "AS IS" BASIS,          #
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+#   See the License for the specific language governing permissions and        #
+#   limitations under the License.                                             #
+################################################################################
+
+helm repo add openebs https://openebs.github.io/openebs
+helm repo update
+helm upgrade --install openebs --namespace openebs openebs/openebs 
+    --version 4.3.0 
+    --create-namespace 
+    --set engines.replicated.mayastor.enabled=false 
+    --set engines.local.lvm.enabled=false 
+    --set engines.local.zfs.enabled=false 
+    --set loki.enabled=false 
+    --set alloy.enabled=false 
+    --wait
+
+kubectl patch storageclass openebs-hostpath -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
